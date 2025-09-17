affa1bd6b5a7228d27d82b1cd1be67e0e3717cca
caonidaye568 <1356508785@qq.com>
2025-09-17 20:19:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ get_current_dayofweek = lambda action: (
 )
 
 SLEEPTIME = 0.1
-RESERVE_TARGET_TIME = "20:19:00"
+RESERVE_TARGET_TIME = "20:21:00"
 ENABLE_SLIDER = True
 MAX_ATTEMPT = 1
 RESERVE_NEXT_DAY = False
